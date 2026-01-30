29823bed1faa744a2dd24847178eafd5944829cc
Balachandar <balachandar10301@gmail.com>
2026-01-31 02:33:28 +0530

    Update joint_space_controller.py

--- cartesian_teleop_controller/cartesian_teleop_controller/joint_space_controller.py
+++ cartesian_teleop_controller/cartesian_teleop_controller/joint_space_controller.py
@@ -8,9 +8,7 @@ from math import pi
 
 # Joint names as in URDF
 JOINT_NAMES = [
-    'joint_1', 'joint_2', 'joint_3',
-    'joint_4', 'joint_5', 'joint_6'
-]
+    'joint_1', 'joint_2', 'joint_3','joint_4', 'joint_5', 'joint_6']
 
 
 class JointSpaceController(Node):
