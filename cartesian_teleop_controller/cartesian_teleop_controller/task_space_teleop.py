495c7a589edbc9fea6fb0d93032aa5351f4dd6b4
Balachandar <balachandar10301@gmail.com>
2026-01-31 02:34:45 +0530

    Fix comment formatting and simplify JOINT_NAMES

--- cartesian_teleop_controller/cartesian_teleop_controller/task_space_teleop.py
+++ cartesian_teleop_controller/cartesian_teleop_controller/task_space_teleop.py
@@ -18,11 +18,9 @@ from scipy.spatial.transform import Rotation as R
 
 
 
-# Joint names as in URDF)
+# Joint names as in URDF
 JOINT_NAMES = [
-    'joint_1', 'joint_2', 'joint_3',
-    'joint_4', 'joint_5', 'joint_6'
-]
+    'joint_1', 'joint_2', 'joint_3','joint_4', 'joint_5', 'joint_6']
 
 # keyboard read 
 def get_key():
